f3a521633655cef51b5be3c29631b258c946d2f7
Lewis Blunn <mbcx9lb3@googlemail.com>
2021-05-20 17:44:33 +0100

    Updated range of dates in the *_data_* files

--- SUEWS-SourceCode/src/suews_ctrl_driver.f95
+++ SUEWS-SourceCode/src/suews_ctrl_driver.f95
@@ -4008,7 +4008,7 @@ CONTAINS
       top_net_lw_spc = lw_flux%top_net(1,1)
       ground_net_lw_spc = lw_flux%ground_net(1,1)
       top_dn_lw_spc = lw_flux%top_dn(1,1)
-
+      PRINT *,'ldown',ldown
       ! sw arrays
       clear_air_abs_sw_spc = 0.0
       clear_air_abs_sw_spc(:nlayers) = sw_flux%clear_air_abs(1,:)
