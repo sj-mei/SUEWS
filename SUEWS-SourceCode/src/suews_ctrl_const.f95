129df8cb543d5d3693e5d274d26592b31c733b12
Ting Sun <sunting.05@gmail.com>
2021-05-21 15:07:24 +0100

    add test for 2021a

--- SUEWS-SourceCode/src/suews_ctrl_const.f95
+++ SUEWS-SourceCode/src/suews_ctrl_const.f95
@@ -956,7 +956,7 @@ MODULE data_in
 
    IMPLICIT NONE
 
-   CHARACTER(len=90)::progname = 'SUEWS_V2020b'
+   CHARACTER(len=90)::progname = 'SUEWS_V2021a'
 
    ! ---- Run information ------------------------------------------------------------------------
    CHARACTER(len=20)::  FileCode   !Set in RunControl
