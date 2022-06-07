710a7dd3c972a79ed8b19f9c55fffc3704f0af97
Ting Sun <sunting.05@gmail.com>
2022-06-07 23:23:20 +0100

    negative z0m values when areaZh>.8 for roughnessmommethod=4 Fixes #107

--- SUEWS-SourceCode/src/suews_phys_resist.f95
+++ SUEWS-SourceCode/src/suews_phys_resist.f95
@@ -486,7 +486,10 @@ CONTAINS
          ELSEIF (RoughLenMomMethod == 4) THEN ! lambdaP dependent as in Fig.1a of G&O (1999)
             ! these are derived using digitalised points
             zdm = (-0.182 + 0.722*sigmoid(-1.16 + 3.89*areaZh) + 0.493*sigmoid(-5.17 + 32.7*areaZh))*Zh
-            z0m = (0.00208 + 0.0165*areaZh + 2.52*areaZh**2 + 3.21*areaZh**3 - 43.6*areaZh**4 + 76.5*areaZh**5 - 40.*areaZh**6)*Zh
+            z0m = (0.00208 + &
+            0.0165*min(areaZh,.7) + 2.52*min(areaZh,.7)**2 + &
+            3.21*min(areaZh,.7)**3 - 43.6*min(areaZh,.7)**4 + &
+            76.5*min(areaZh,.7)**5 - 40.*min(areaZh,.7)**6)*Zh
          END IF
       ELSEIF (Zh == 0) THEN !If zh calculated to be zero, set default roughness length and displacement height
          IF (areaZh /= 0) CALL ErrorHint(15, 'In SUEWS_RoughnessParameters.f95, zh = 0 m but areaZh > 0', zh, areaZh, notUsedI)
