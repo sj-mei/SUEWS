7f14dc9c7032eaa9e121b08ae70e3706f09d44a8
Ting Sun <sunting.05@gmail.com>
2022-09-08 23:41:00 +0100

    more fix

--- supy-driver/build-win-def.py
+++ supy-driver/build-win-def.py
@@ -14,18 +14,19 @@ print("Fixing def file for Windows")
 
 dll_file = find_python_dll()
 p_dll=Path(dll_file)
-print('here is the dll:', dll_file)
-# log.info('Building import library (arch=AMD64): "%s" (from %s)' %
-#             (out_file, dll_file))
+print('Here is the pythonlib.dll:', dll_file)
 
 # generate symbol list from this library
 def_name = "python%d%d.def" % tuple(sys.version_info[:2])
 def_file = os.path.join(sys.prefix, 'libs', def_name)
-Path(def_file).parent.mkdir(parents=True, exist_ok=True)
-print('ok to create? ', Path(def_file).parent.exists())
-# print('ok to create? ', Path(def_file).parent.exists())
 
-# print('sleeping for 999 seconds')
-# sleep(999)
+# fix path for def file
+Path(def_file).parent.mkdir(parents=True, exist_ok=True)
+print('OK to create? ', Path(def_file).parent.exists())
+
+# t_sleep_sec=1
+# print(f'sleeping for {t_sleep_sec} seconds')
+# sleep(t_sleep_sec)
+
 
 generate_def(dll_file, def_file)
