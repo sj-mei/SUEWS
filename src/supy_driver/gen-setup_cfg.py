37447ddc82894bee32ec5225c00f2191735d96c1
Ting Sun <sunting.05@gmail.com>
2023-04-02 21:10:22 +0100

    try to fix windows issue on GH actions

---
+++ src/supy_driver/gen-setup_cfg.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import platform
+
+
+sysname = platform.system()
+lib_basename = "supy_driver"
+if sysname == "Windows":
+    lib_name = lib_basename + ".pyd"
+    Path("setup.cfg").write_text(
+        "[build_ext]\ncompiler=mingw32\n[build]\ncompiler=mingw32\n"
+    )
+    print("setup.cfg created")
+    print(list(Path.cwd().glob("*")))
+elif sysname == "Darwin":
+    lib_name = lib_basename + ".so"
+elif sysname == "Linux":
+    lib_name = lib_basename + ".so"
