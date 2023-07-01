a89a00dfdf16f5bc89b03902d1e98ed9aafd5e8f
Ting Sun <sunting.05@gmail.com>
2023-07-01 21:46:25 +0100

    Fast spin-up for large-scale simulations Fixes #200

--- src/supy/supy/util/__init__.py
+++ src/supy/supy/util/__init__.py
@@ -54,3 +54,5 @@ from ._wrf import extract_reclassification, plot_reclassification
 from ._roughness import cal_z0zd, cal_neutral
 
 from ._debug import diag_rsl, diag_rsl_prm, save_zip_debug
+
+from ._spinup import get_spinup_state
