ce7475f5fbc4967daa7b714be46a692cbb5ec99d
Ting Sun <sunting.05@gmail.com>
2023-08-30 21:29:43 +0100

    fix .applymap for pd.ver<2.1

--- src/supy/supy/_post.py
+++ src/supy/supy/_post.py
@@ -11,7 +11,13 @@ def get_output_info_df():
     var_list_x = [np.array(sd.output_name_n(i)) for i in np.arange(size_var_list) + 1]
 
     df_var_list = pd.DataFrame(var_list_x, columns=["var", "group", "aggm", "outlevel"])
-    df_var_list = df_var_list.map(lambda x: x.decode().strip())
+    if pd.__version__ >= "2.1.0":
+        # if pandas version is 2.1.0 or above, we can use `df.map`
+        df_var_list = df_var_list.map(lambda x: x.strip())
+    else:
+        # otherwise, we need to use `df.applymap`
+        df_var_list = df_var_list.applymap(lambda x: x.decode().strip())
+
     df_var_list_x = df_var_list.replace(r"^\s*$", np.nan, regex=True).dropna()
     var_dfm = df_var_list_x.set_index(["group", "var"])
     return var_dfm
