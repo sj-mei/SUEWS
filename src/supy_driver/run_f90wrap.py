47197e4a90bf1edd2a49cce83720457c7f7856db
Ting Sun <sunting.05@gmail.com>
2024-05-15 17:02:27 +0100

    chore: Update test-meson workflow to exclude building wheels and setup tmate session for debugging

---
+++ src/supy_driver/run_f90wrap.py
@@ -0,0 +1,58 @@
+import subprocess
+import sys
+import os
+
+
+def main():
+    f90wrap_executable = sys.argv[1]  # path to f2py or f2py part of numpy
+    module_name = sys.argv[2]
+    current_source_dir = sys.argv[3]
+    output_dir = sys.argv[4]
+    input_output_files = sys.argv[5:]
+    # only .fpp files are input files
+    input_files = [f for f in input_output_files if f.endswith(".fpp")]
+    # only .f90 files are output files
+    output_files = [f for f in input_output_files if f.endswith(".f90")]
+    # input_files = input_output_files[0:-2]
+    # output_files = input_output_files[-2:]
+    # output_files = sys.argv[5:]  # List of output file paths
+
+    # Call f2py to generate the modules
+    try:
+        subprocess.check_call(
+            [
+                f90wrap_executable,
+                "-m",
+                module_name,
+                *input_files,
+                "-k",
+                os.path.join(current_source_dir, "kind_map"),
+                "--skip",
+                "error_hint",
+            ]
+        )
+        print("f90wrap call successful")
+    except subprocess.CalledProcessError as e:
+        print(f"Error calling f2py: {e}")
+        return 1
+
+    # Move generated files to the output directory
+    try:
+        subprocess.check_call(
+            [
+                "python",
+                os.path.join(current_source_dir, "move_output_gen.py"),
+                *output_files,
+                output_dir,
+            ]
+        )
+        print("Output files moved successfully")
+    except subprocess.CalledProcessError as e:
+        print(f"Error moving output files: {e}")
+        return 1
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
