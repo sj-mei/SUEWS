5c68f300f738279c50a83824e33cb9d56194921d
matthewp <m.paskin@pgr.reading.ac.uk>
2024-09-17 10:43:16 +0100

    added stebbs runcontrol data

--- src/supy/_load.py
+++ src/supy/_load.py
@@ -1331,8 +1331,19 @@ def load_SUEWS_dict_ModConfig(path_runcontrol, dict_default=dict_RunControl_defa
         / dict_RunControl["fileinputpath"]
         / "SUEWS_SPARTACUS.nml"
     )
+
+    # load STEBBS-specific variables:
+    path_stebbs = (
+        path_runcontrol.parent
+        / dict_RunControl["fileinputpath"]
+        / "SUEWS_STEBBS.nml"
+    )
+
     dict_RunControl_x = {k[0]: v for k, v in load_SUEWS_nml(path_spartacus).items()}
     dict_RunControl.update(dict_RunControl_x)
+
+    dict_RunControl_y = {k[0]: v for k, v in load_SUEWS_nml(path_stebbs).items()}
+    dict_RunControl.update(dict_RunControl_y)
 
     return dict_RunControl
 
