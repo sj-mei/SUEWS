12f074e0541d8bf8ad9757b82d82f709211a807d
Ting Sun <sunting.05@gmail.com>
2024-11-12 22:14:33 +0000

    fix: Restore valid range checks for albedo values in update_Veg subroutine

--- src/suews/src/suews_phys_dailystate.f95
+++ src/suews/src/suews_phys_dailystate.f95
@@ -1202,12 +1202,13 @@ CONTAINS
       porosity_id = MIN(MAX(porosity_id, MAX(PorMin_dec, 0.1)), MIN(PorMax_dec, 0.9)) ! limit porosity to valid range, TS 26 Jun 2023
 
       ! update albedo values while limiting these to valid ranges
-      !albDecTr_id = min(max(albDecTr_id + albChangeDecTr, AlbMin_DecTr), AlbMax_DecTr)
-      !albEveTr_id = min(max(albEveTr_id + albChangeEveTr, AlbMin_EveTr), AlbMax_EveTr)
-      !albGrass_id = min(max(albGrass_id + albChangeGrass, AlbMin_Grass), AlbMax_Grass)
-      albDecTr_id = albDecTr_id + albChangeDecTr
-      albEveTr_id = albEveTr_id + albChangeEveTr
-      albGrass_id = albGrass_id + albChangeGrass
+      albDecTr_id = min(max(albDecTr_id + albChangeDecTr, AlbMin_DecTr), AlbMax_DecTr)
+      albEveTr_id = min(max(albEveTr_id + albChangeEveTr, AlbMin_EveTr), AlbMax_EveTr)
+      albGrass_id = min(max(albGrass_id + albChangeGrass, AlbMin_Grass), AlbMax_Grass)
+      ! TS 12 Nov 2024: revert the behaviour in restricting albDecTr_id, albEveTr_id, albGrass_id to valid ranges #285
+      ! albDecTr_id = albDecTr_id + albChangeDecTr
+      ! albEveTr_id = albEveTr_id + albChangeEveTr
+      ! albGrass_id = albGrass_id + albChangeGrass
 
    END SUBROUTINE update_Veg
 
