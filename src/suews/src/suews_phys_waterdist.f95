e881c8ccdc6fc58769f3f2d109077d9d151e8598
sunt05 <sunt05@users.noreply.github.com>
2025-01-23 16:56:04 +0000

    [GH Actions] fprettify source code

--- src/suews/src/suews_phys_waterdist.f95
+++ src/suews/src/suews_phys_waterdist.f95
@@ -807,20 +807,19 @@ CONTAINS
                IF (SnowUse == 0) THEN
                   !No snow calculations!
                   AddWater(i_receiver) = AddWater(i_receiver) + &
-                     (Drain(i_contributor)*sfr_surf(i_contributor) &
-                     /sfr_surf(i_receiver))*WaterDist(i_receiver, i_contributor) !Original
-
+                                         (Drain(i_contributor)*sfr_surf(i_contributor) &
+                                          /sfr_surf(i_receiver))*WaterDist(i_receiver, i_contributor) !Original
 
                ELSE
                   !Snow included, This needs to be fixed at some point. LJ Mar 2013
                   AddWaterRunoff(i_contributor) = AddWaterRunoff(i_contributor) &
-                  + WaterDist(i_receiver, i_contributor) !No receiving surface -> runoff
+                                                  + WaterDist(i_receiver, i_contributor) !No receiving surface -> runoff
                END IF
 
             ELSE
                !If no receiving surface exists, water fraction goes to AddWaterRunoff
                AddWaterRunoff(i_contributor) = AddWaterRunoff(i_contributor) &
-                  + WaterDist(i_receiver, i_contributor)
+                                               + WaterDist(i_receiver, i_contributor)
             END IF
          END DO
       END DO
