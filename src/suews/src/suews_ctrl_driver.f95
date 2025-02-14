3cd2b7d6e77c231daf782e82a386bfeec83c6f43
Ting Sun <sunting.05@gmail.com>
2025-02-14 17:13:39 +0000

    Remove unused SUEWS_cal_RoughnessParameters subroutine
    - Delete the full implementation of SUEWS_cal_RoughnessParameters from suews_phys_resist.f95
    - Remove references to this subroutine in suews_ctrl_driver.f95 and suews_phys_rslprof.f95
    - Retain the SUEWS_cal_RoughnessParameters_DTS subroutine for further use

--- src/suews/src/suews_ctrl_driver.f95
+++ src/suews/src/suews_ctrl_driver.f95
@@ -25,7 +25,7 @@ MODULE SUEWS_Driver
    USE SPARTACUS_MODULE, ONLY: SPARTACUS
    ! USE AnOHM_module, ONLY: AnOHM
    USE resist_module, ONLY: AerodynamicResistance, BoundaryLayerResistance, SurfaceResistance, &
-                            SUEWS_cal_RoughnessParameters, SUEWS_cal_RoughnessParameters_DTS
+                            SUEWS_cal_RoughnessParameters_DTS
    USE OHM_module, ONLY: OHM
    USE ESTM_module, ONLY: ESTM
    USE EHC_module, ONLY: EHC
